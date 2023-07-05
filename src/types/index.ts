04f9b765a5cddfb8ebe51066abc752b340907f26
TT-RR <kaiza1mage@gmail.com>
2023-07-05 10:38:09 +0900

    import package

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+//型の宣言
+export type Todo = {
+    id: number,
+    title: string,
+    created_at: Date,
+    updated_at: Date
+}
+export type CsrfToken = {
+    csrf_token: string
+}
+export type Credential = {
+    email: string,
+    password: string
+}
