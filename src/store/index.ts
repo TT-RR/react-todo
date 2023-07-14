1ac5de33a08e8b5f03b8d166c5fc6c3652bd8511
TT-RR <kaiza1mage@gmail.com>
2023-07-14 09:39:20 +0900

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,3 +4,20 @@ type EditedTask = {
     id: number
     title: string
 }
+
+type State = {
+    editedTask: EditedTask,
+    updateEditedTask: (payload: EditedTask) => void
+    resetEditedTask: () => void
+}
+
+const useStore = create<State>((set) => ({
+    editedTask: { id: 0, title: '' },
+    updateEditedTask: (payload) =>
+        set({
+            editedTask: payload
+        }),
+    resetEditedTask: () => set({ editedTask: { id: 0, title: '' } }),
+}))
+
+export default useStore
